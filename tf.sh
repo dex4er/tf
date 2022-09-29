b0cb7f95848935d8da51e9384d2888343551347a
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2022-09-30 00:27:35 +0200

    tf init: grep --line-buffered

--- tf.sh
+++ tf.sh
@@ -331,7 +331,7 @@ init)
   set -e
 
   exec terraform init -upgrade | eval "$logging" |
-    grep -v -P 'Finding .* versions matching|Initializing (modules|the backend|provider plugins)...|Upgrading modules...|Using previously-installed|Reusing previous version of|from the shared cache directory|in modules/' |
+    grep --line-buffered -v -P 'Finding .* versions matching|Initializing (modules|the backend|provider plugins)...|Upgrading modules...|Using previously-installed|Reusing previous version of|from the shared cache directory|in modules/' |
     sed '/Terraform has been successfully initialized/,$d' |
     uniq |
     sed '1{/^$/d}'
