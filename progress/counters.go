8d7b9a139e22d50c676a0df7213d27dfc3d9b4ea
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2023-08-04 11:50:51 +0200

    Math moved to util

--- progress/counters.go
+++ progress/counters.go
@@ -3,8 +3,10 @@ package progress
 import (
 	"fmt"
 
-	"github.com/dex4er/tf/console"
 	"github.com/mitchellh/colorstring"
+
+	"github.com/dex4er/tf/console"
+	"github.com/dex4er/tf/util"
 )
 
 var refreshed = 0
@@ -13,50 +15,36 @@ var stopped = map[string]int{"R": 0, "C": 0, "D": 0, "M": 0}
 
 func refreshCounters(line string, resource string, operation string) {
 	refreshed += 1
-	showCounter(line, resource, operation)
+	showCounters(line, resource, operation)
 }
 
 func startCounters(line string, resource string, operation string) {
 	started[operation] += 1
-	showCounter(line, resource, operation)
+	showCounters(line, resource, operation)
 }
 
 func stillCounters(line string, resource string, operation string) {
-	showCounter(line, resource, operation)
+	showCounters(line, resource, operation)
 }
 
 func stopCounters(line string, resource string, operation string) {
 	stopped[operation] += 1
-	showCounter(line, resource, operation)
+	showCounters(line, resource, operation)
 }
 
-func showCounter(line string, resource string, operation string) {
+func showCounters(line string, resource string, operation string) {
 	s := fmt.Sprintf("^%d", refreshed)
 	r := fmt.Sprintf("=%d/%d", stopped["R"], started["R"])
 	c := fmt.Sprintf("+%d/%d", stopped["C"], started["C"])
 	m := fmt.Sprintf("~%d/%d", stopped["M"], started["M"])
 	d := fmt.Sprintf("-%d/%d", stopped["D"], started["D"])
 
-	maxLine := maxInt(console.Cols-len(s)-len(r)-len(c)-len(m)-len(d)-6, 0)
-	l := line[:minInt(len(line), maxLine)]
+	maxLine := util.MaxInt(console.Cols-len(s)-len(r)-len(c)-len(m)-len(d)-6, 0)
+	l := line[:util.MinInt(len(line), maxLine)]
 
 	if NoColor {
 		fmt.Printf("%s %s %s %s %s %s\r", s, r, c, m, d, l)
 	} else {
 		colorstring.Printf("[blue]%s[reset] [cyan]%s[reset] [green]%s[reset] [yellow]%s[reset] [red]%s[reset] %s\r", s, r, c, m, d, l)
 	}
-}
-
-func maxInt(x int, y int) int {
-	if x > y {
-		return x
-	}
-	return y
-}
-
-func minInt(x int, y int) int {
-	if x < y {
-		return x
-	}
-	return y
 }
---
+++ util/math.go
@@ -0,0 +1,15 @@
+package util
+
+func MaxInt(x int, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func MinInt(x int, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
