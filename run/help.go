f84801f1739b0f8ce261c85c1259f578c58a540c
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2023-08-05 22:19:45 +0200

    New commands in help message

--- run/help.go
+++ run/help.go
@@ -3,10 +3,13 @@ package run
 import "fmt"
 
 func Help(args []string) error {
+	// should provoke an error
 	err := Terraform("", args)
 
 	fmt.Println()
 	fmt.Println("Extra commands:")
+	fmt.Println("  mv            The same as terraform state mv")
+	fmt.Println("  rm            The same as terraform state rm")
 	fmt.Println("  upgrade       The same as terraform init -upgrade")
 
 	return err
