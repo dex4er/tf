531c0d28353141dbcfafebc1c85a1eac53763be6
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2023-08-06 11:51:57 +0200

    TF_OUTPUT_PATH has strftime format

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/dex4er/tf
 go 1.20
 
 require (
+	github.com/awoodbeck/strftime v0.0.0-20180221155908-016cde65fcde
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db
 	golang.org/x/term v0.11.0
 )
--- util/output.go
+++ util/output.go
@@ -3,12 +3,17 @@ package util
 import (
 	"fmt"
 	"os"
+	"time"
+
+	"github.com/awoodbeck/strftime"
 )
 
 // Opens log file if `TF_OUTPUT_PATH` environment variable is set
 func OpenOutputFile() (*os.File, error) {
-	if logFilename := os.Getenv("TF_OUTPUT_PATH"); logFilename != "" {
-		file, err := os.OpenFile(logFilename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if outputPath := os.Getenv("TF_OUTPUT_PATH"); outputPath != "" {
+		now := time.Now()
+		filename := strftime.Format(&now, outputPath)
+		file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 		if err != nil {
 			return nil, fmt.Errorf("opening the log file: %w", err)
 		}
