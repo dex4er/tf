8c0b71833e7a972b161132bd3aeba457584a6a24
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-01-02 21:14:53 +0100

    CI: wrapper: false

--- demo.tf
+++ demo.tf
@@ -8,6 +8,17 @@
 
 terraform {
   required_version = "~> 1.5"
+
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.12"
+    }
+  }
 }
 
 variable "n" {
