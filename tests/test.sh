b74131dc71ed709c7e350921deb6832395b39940
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2026-05-21 18:15:00 +0200

    test fix

--- tests/test.sh
+++ tests/test.sh
@@ -11,15 +11,15 @@ mkdir -p $TF_PLUGIN_CACHE_DIR
 status=0
 
 has_terraform=$(command -v terraform >/dev/null && echo yes || echo no)
-has_tofu=$(command -v terraform >/dev/null && echo yes || echo no)
+has_tofu=$(command -v tofu >/dev/null && echo yes || echo no)
 
 for t in [0-9]*.sh; do
   echo -n "Testing $t... "
-  if [[ ${t} == *terraform.out && $has_terraform == no ]]; then
+  if [[ ${t} == *_terraform.sh && ${has_terraform} == no ]]; then
     echo -e "\033[0mSKIPPED (terraform not found)"
     continue
   fi
-  if [[ ${t} == *tofu.out && $has_tofu == no ]]; then
+  if [[ ${t} == *_tofu.sh && ${has_tofu} == no ]]; then
     echo -e "\033[0mSKIPPED (tofu not found)"
     continue
   fi
