4dbdcee1edd3e40ce6b071679878cd5798539741
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2026-05-21 18:10:58 +0200

    Separate test for terraform and tofu

--- tests/test.sh
+++ tests/test.sh
@@ -10,8 +10,19 @@ mkdir -p $TF_PLUGIN_CACHE_DIR
 
 status=0
 
+has_terraform=$(command -v terraform >/dev/null && echo yes || echo no)
+has_tofu=$(command -v terraform >/dev/null && echo yes || echo no)
+
 for t in [0-9]*.sh; do
   echo -n "Testing $t... "
+  if [[ ${t} == *terraform.out && $has_terraform == no ]]; then
+    echo -e "\033[0mSKIPPED (terraform not found)"
+    continue
+  fi
+  if [[ ${t} == *tofu.out && $has_tofu == no ]]; then
+    echo -e "\033[0mSKIPPED (tofu not found)"
+    continue
+  fi
   if bash $t </dev/null 1>&2; then
     echo -e "\033[0mOK"
   else
