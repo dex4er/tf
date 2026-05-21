88a71e6c3e3778fd36cacbcce4244cbd533afeb5
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2026-05-21 18:24:25 +0200

    New option `tf apply -plan=FILE` (#112)

---
+++ tests/37_apply_plan_terraform.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd "$(dirname "$0")"
+test=$(basename "$0" .sh)
+
+export TERRAFORM_PATH=${test##*_}
+
+rm -rf tmp/$test
+mkdir -p tmp/$test
+cp ../demo.tf tmp/$test
+pushd tmp/$test >/dev/null
+
+{
+  set -x
+  ../../../tf init
+  ../../../tf upgrade
+  ../../../tf plan -parallelism=30 -out=tfplan1
+  ../../../tf apply -auto-approve -parallelism=30 -plan=tfplan1
+  ../../../tf refresh -parallelism=30
+  ../../../tf plan -destroy -parallelism=30 -out=tfplan2
+  ../../../tf apply -auto-approve -parallelism=30 -plan=tfplan2
+} 2>&1 | ../../sanitize.sh >>tf.out
+
+diff -u ../../$test.out tf.out
+
+popd >/dev/null
+
+rm -rf tmp/$test
